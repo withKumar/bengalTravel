9dda6982eb672483ac4109e5e020e955ff2484a4
Amartya <authenticamartya@gmail.com>
2020-04-09 02:28:56 +0530

    Body

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/header/header.component';
 import { NavbarComponent } from './component/navbar/navbar.component';
 import { BannerComponent } from './component/banner/banner.component';
+import { BodyComponent } from './component/body/body.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     NavbarComponent,
-    BannerComponent
+    BannerComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule,
