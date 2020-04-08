5d418e8b8d98ce5115f48cf4afd82f02375e8375
Amartya <authenticamartya@gmail.com>
2020-04-09 01:51:15 +0530

    Header Navbar Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { headerComponent } from './component/header/header.component';
+import { navbarComponent } from './component/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    headerComponent,
+    navbarComponent
   ],
   imports: [
     BrowserModule,
